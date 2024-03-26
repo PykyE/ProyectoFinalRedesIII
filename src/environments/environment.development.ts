65b10108d4cab236446d8ae6f6b9c313521f0ea9
Miguel Rico <miguel35697@hotmail.com>
2024-03-25 21:58:17 -0500

    [REMOVE] env vars

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,2 @@
 export const environment = {
-  production: false,
-  lambda_url: "",
-  g_maps_api_key: ""
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,2 @@
 export const environment = {
-  production: true,
-  lambda_url: "",
-  g_maps_api_key: ""
 };
